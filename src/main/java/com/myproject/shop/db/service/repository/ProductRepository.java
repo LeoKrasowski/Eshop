51ef3fd4104616fa12544b70a389ed68358c1370
HomePC <leokrasowski@gmail.com>
2023-05-28 21:56:39 +0200

    Added Product Repository operations

--- src/main/java/com/myproject/shop/db/service/repository/ProductRepository.java
+++ src/main/java/com/myproject/shop/db/service/repository/ProductRepository.java
@@ -1,10 +1,23 @@
 package com.myproject.shop.db.service.repository;
 
+import com.myproject.shop.db.service.API.request.UpdateProductRequest;
 import com.myproject.shop.db.service.mapper.ProductRowMapper;
 import com.myproject.shop.domain.Product;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+@Component
 public class ProductRepository {
     private final JdbcTemplate jdbcTemplate;
     private final ProductRowMapper productRowMapper = new ProductRowMapper();
@@ -20,5 +33,46 @@ public class ProductRepository {
             } catch (EmptyResultDataAccessException e) {
                 return null;
             }
+    }
+
+    public Integer add (Product product){
+        final String sql = "insert into product (merchant_id, name, description, price, created_at, available) values (?,?,?,?,?,?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, product.getMerchantId());
+                ps.setString(2, product.getName());
+                ps.setString(3, product.getDescription());
+                ps.setDouble(4, product.getPrice());
+                ps.setTimestamp(5, product.getCreatedAt());
+                ps.setInt(6,product.getAvailable());
+                return ps;
+            }
+        }, keyHolder);
+
+        if(keyHolder.getKey()!=null){
+            return keyHolder.getKey().intValue();
+        } else {
+            return null;
+        }
+    }
+
+    public List<Product> getAll(){
+
+        final String sql = "select * from product";
+        return jdbcTemplate.query(sql, productRowMapper);
+    }
+
+    public void update(int id, UpdateProductRequest request){
+        final String sql = "update product srt name = ?, description = ?, price = ?, available = ? where id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getDescription(), request.getPrice(), request.getAvailable());
+    }
+
+    public void delete (int id){
+        final String sql = "delete from product where id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
